e590275b99f37890ecfd10eaac793675fa567ead
GeraskoNikita <nikitagerasko@mail.ru>
2025-08-17 14:55:37 +0600

    Update HW_4.java

--- src/HW_4.java
+++ src/HW_4.java
@@ -1,18 +1,18 @@
 import java.util.Random;
 
-public class HW_4 {
-    public static int bossHealth = 1000;
+public class Lesson_4 {
+    public static int bossHealth = 700;
     public static int bossDamage = 50;
     public static String bossDefence;
-    public static int medicHealth = 500;
-    public static double healPower = 0;
-    public static long[] heroesHealth = {280, 270, 250};
+    public static int medicHealth = 450;
+    public static int healAmount = 50;
+    public static int[] heroesHealth = {280, 270, 250};
     public static int[] heroesDamage = {20, 15, 10};
     public static String[] heroesAttackType = {"Physical", "Magical", "Kinetic"};
     public static int roundNumber;
 
     public static void main(String[] args) {
-        // printStatistics();
+        // printStatistics(); перенёс на строку 43
         while (!isGameOver()) {
             playRound();
         }
@@ -42,8 +42,12 @@ public class HW_4 {
         chooseBossDefence();
         printStatistics();
         bossAttack();
+        if (medicHealth > 0) {
+            randomCureHero();
+        }
+
         heroesAttack();
-        // printStatistics();
+        printStatistics();
     }
 
     public static void chooseBossDefence() {
@@ -52,27 +56,43 @@ public class HW_4 {
         bossDefence = heroesAttackType[randomIndex];
     }
 
-    public static void heroesAttack() {
-        if (medicHealth > 0) {
-            for (int i = 0; i < heroesHealth.length; i++) {
-                if (heroesHealth[i] < 100) { // Исцеляем ели здоровье мегьше 100
-                    Random rnd = new Random();
-                    healPower = rnd.nextDouble(2.0);
-                    System.out.println("Исцелен " + heroesAttackType[i] + " на " + Math.round(heroesHealth[i] * healPower) + " единиц , коэфицент  " + healPower);
-                    heroesHealth[i] = heroesHealth[i] + Math.round(heroesHealth[i] * healPower);
+    public static void randomCureHero() {
+        /* РАНДОМНО ВЫБИРАЕМ ГЕРОЯ КОТОРОГО НАДО ЛЕЧИТЬ */
 
-                }
+
+        int[] wounded = new int[heroesHealth.length]; // Собираем индексы героев у которых здоровте меньше 100 единиц в массив фиксированной длины
+        int woundedCount = 0;
+        for (int i = 0; i < heroesHealth.length; i++) {
+            if (heroesHealth[i] > 0 && heroesHealth[i] < 100) { // жив и не полный хп
+                wounded[woundedCount++] = i;  // запоминаем индекс
             }
         }
+
+
+
+        if (woundedCount > 0) {
+            Random rnd = new Random();
+            int targetIndex = wounded[rnd.nextInt(woundedCount)]; // Выбираем случайного героя
+
+            System.out.println("Медик вылечил воина с  " + heroesAttackType[targetIndex] +
+                    ": " + heroesHealth[targetIndex] + " → " + Math.min(100, heroesHealth[targetIndex] + healAmount));
+
+            heroesHealth[targetIndex] = Math.min(100, heroesHealth[targetIndex] + healAmount); // Лечим героя
+
+
+        }
+
+    }
+
+    public static void heroesAttack() {
         for (int i = 0; i < heroesDamage.length; i++) {
             if (heroesHealth[i] > 0 && bossHealth > 0) {
-
                 int damage = heroesDamage[i];
                 if (bossDefence == heroesAttackType[i]) {
                     Random random = new Random();
                     int coef = random.nextInt(9) + 2; // 2,3,4,5,6,7,8,9,10
                     damage *= coef;
-                    System.out.println("Critical damage " + heroesAttackType[i] + " : " + damage);
+                    System.out.println("Critical damage: " + damage);
                 }
                 bossHealth = bossHealth - damage;
                 if (bossHealth < 0) {
@@ -86,14 +106,11 @@ public class HW_4 {
         for (int i = 0; i < heroesHealth.length; i++) {
             if (heroesHealth[i] > 0) {
                 heroesHealth[i] = heroesHealth[i] - bossDamage;
-
-
                 if (heroesHealth[i] < 0) {
                     heroesHealth[i] = 0;
                 }
             }
         }
-
         if (medicHealth > 0) {
             medicHealth = medicHealth - bossDamage;
         }
@@ -101,14 +118,10 @@ public class HW_4 {
 
     public static void printStatistics() {
         System.out.println("ROUND " + roundNumber + " -------------");
-        /*String defence;
-        if (bossDefence == null) {
-            defence = "No defence";
-        } else {
-            defence = bossDefence;
-        }*/
+
         System.out.println("Boss health: " + bossHealth + " damage: " + bossDamage
                 + " defence: " + (bossDefence == null ? "No defence" : bossDefence));
+        System.out.println("Medic health: " + medicHealth);
         for (int i = 0; i < heroesAttackType.length; i++) {
             System.out.println(heroesAttackType[i]
                     + " health: " + heroesHealth[i]
@@ -116,5 +129,3 @@ public class HW_4 {
         }
     }
 }
-
-
